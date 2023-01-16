925f3bd9216742e564c829869c1a4404059c8d59
alexeiastrakh <92253303+alexeiastrakh@users.noreply.github.com>
2023-01-16 18:35:12 +0200

    Add files via upload

---
+++ ConsoleApp1/UpdateInventories.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    class UpdateInventories
+    {
+        public static void updateInventories()
+        {
+         
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load(@"../../../OrdersFile/NewOrders.xml");
+            XmlNodeList listQtyOdered = xdoc.SelectNodes("//qtyOrdered");
+            XmlNodeList listDescription = xdoc.SelectNodes("//description");
+            XmlNodeList listUnitCost = xdoc.SelectNodes("//unitCost");
+            List<String> UpdateInventoriesElements = new List<String>();
+            List<String> StringUpdateInventoriesTegs = new List<String>();
+            int counter = 0;
+
+            while (counter != listQtyOdered.Count)
+            {
+                UpdateInventoriesElements.Add(listQtyOdered.Item(counter).InnerText);
+                UpdateInventoriesElements.Add(listDescription.Item(counter).InnerText);
+                UpdateInventoriesElements.Add(listUnitCost.Item(counter).InnerText);
+
+                StringUpdateInventoriesTegs.Add("qtyOrdered");
+                StringUpdateInventoriesTegs.Add("description");
+                StringUpdateInventoriesTegs.Add("unitCost");
+              
+
+
+
+                counter++;
+            }
+            int counterUpdateInventoriesString = 0;
+            List<String> data = UpdateInventoriesElements;
+            XElement root = new XElement("INVRPT",
+                                        from item in data
+                                        select new XElement(StringUpdateInventoriesTegs[counterUpdateInventoriesString++], item));
+
+            root.Save(@"../../../EdiGeneratedFiles/UpdateInventories.INVRPT");
+
+
+
+        }
+    }
+
+
+
+    }
+
