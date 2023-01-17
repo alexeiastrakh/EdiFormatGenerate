796dfee00ed3b1c5ba14a8028811a664852e8b26
alexeiastrakh <92253303+alexeiastrakh@users.noreply.github.com>
2023-01-17 16:14:02 +0200

    Add files via upload

---
+++ ConsoleApp1/ParserEdi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace EdiFileGenerator
+{
+    class ParserEdi
+    {
+      public static async Task ParseEdi(String path)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Async = true;
+
+            using (XmlReader reader = XmlReader.Create(path, settings))
+            {
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            Console.WriteLine("Start Element {0}", reader.Name);
+                            break;
+                        case XmlNodeType.Text:
+                            Console.WriteLine("Text Node: {0}",
+                                     await reader.GetValueAsync());
+                            break;
+                        case XmlNodeType.EndElement:
+                            Console.WriteLine("End Element {0}", reader.Name);
+                            break;
+                       
+                    }
+                }
+            }
+        }
+    }
+
+
+    }
+    
+      
+
+   
